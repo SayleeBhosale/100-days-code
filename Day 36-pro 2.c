97cd92ec240f03a7fbfd94100421551183dc986b
SayleeBhosale <sayleebhosale3615@gmail.com>
2025-11-20 23:39:58 +0530

    Add files via upload

---
+++ Day 36-pro 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    int totalSum = 0;
+
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+            totalSum += matrix[i][j]; 
+        }
+    }
+
+    printf("\nSum of all elements in the matrix: %d\n", totalSum);
+
+    return 0;
+}
