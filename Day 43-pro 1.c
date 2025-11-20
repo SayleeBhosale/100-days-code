a9591177c9e3145dd05c38c3a4c3a1d4fcfb5bb0
SayleeBhosale <sayleebhosale3615@gmail.com>
2025-11-21 00:29:43 +0530

    Add files via upload

---
+++ Day 43-pro 1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h> 
+
+void reverseString(char *str) {
+    int length = strlen(str);
+    int i, j;
+    char temp;
+
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main() {
+    char myString[] = "Hello, World!"; 
+    printf("Original string: %s\n", myString);
+
+    reverseString(myString); 
+
+    printf("Reversed string: %s\n", myString);
+
+    return 0;
+}
