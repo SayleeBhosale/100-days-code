47b73b4372d779e99dbf76a8839893c4316385c7
SayleeBhosale <sayleebhosale3615@gmail.com>
2025-11-20 23:47:18 +0530

    Add files via upload

---
+++ Day 38-pro 2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    if (rows != cols) {
+        printf("The matrix is not square, so it cannot be symmetric.\n");
+        return 1; 
+    }
+
+    int matrix[rows][cols];
+
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("\nEntered Matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    int isSymmetric = 1; 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0; 
+                break; 
+            }
+        }
+        if (!isSymmetric) {
+            break; 
+        }
+    }
+
+    if (isSymmetric) {
+        printf("\nThe matrix is Symmetric.\n");
+    } else {
+        printf("\nThe matrix is NOT Symmetric.\n");
+    }
+
+    return 0;
+}
